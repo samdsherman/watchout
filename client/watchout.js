d8a68472cc074ed0ee8920c40d1afb78245f72ad
archcorsair <archcorsair@gmail.com>
2016-09-23 23:48:23 -0700

    Remove unused bits of code, improved readability

--- client/watchout.js
+++ client/watchout.js
@@ -1,38 +1,24 @@
-// // start slingin' some d3 here.
-// var jsonCircles = [
-//   { 'xAxis': 30, 'yAxis': 30, 'radius': 20, 'color' : 'green' },
-//   { 'xAxis': 70, 'yAxis': 70, 'radius': 20, 'color' : 'purple'},
-//   { 'xAxis': 110, 'yAxis': 100, 'radius': 20, 'color' : 'red'}];
-
-// var svgContainer = d3.select('body').append('svg');
-
-// var circles = svgContainer.selectAll('circle')
-//                           .data(jsonCircles)
-//                           .enter()
-//                           .append('circle');
-
-// var circleAttributes = circles
-//                        .attr('cx', function (d) { return d.xAxis; })
-//                        .attr('cy', function (d) { return d.yAxis; })
-//                        .attr('r', function (d) { return d.radius; })
-//                        .style('fill', function(d) { return d.color; });
-
 var board = d3.select('.board').append('svg');
 var defs = board.append('svg:defs');
 defs.append('svg:pattern')
     .attr('id', 'asteroid_img')
     .attr('width', 30)
     .attr('height', 30)
-    //.attr('patternUnits', 'userSpaceOnUse')
     .append('svg:image')
     .attr('xlink:href', 'asteroid.png')
     .attr('width', 30)
-    .attr('height', 30)  
+    .attr('height', 30)
     .attr('x', 0)
     .attr('y', 0);
 
+// Globals
+const numEnemies = 1;
+var transitionTime = 2000;
+var newGame = true;
+var playerX = 0;
+var playerY = 0;
+// End Globals
 
-const numEnemies = 500;
 var playingFieldSize = {
   width: board[0][0].scrollWidth,
   height: board[0][0].scrollHeight
@@ -59,15 +45,15 @@ board.selectAll('.enemy').data(generateCirclePositions(numEnemies))
                          .attr('cx', function (d) { return d.xAxis; })
                          .attr('cy', function (d) { return d.yAxis; })
                          .attr('r', function (d) { return d.radius; })
-                          .style('fill', function(d) { return d.color; })
-                         //.style('fill', 'url(#asteroid_img)')
+                         .style('fill', function(d) { return d.color; })
+                         //.style('fill', 'url(#asteroid_img)') // Replace with image, maybe not use SVG at all for enemy?
                          .classed('enemy', true);
 
 // Make them move!
 var updateEnemyPosition = function() {
   board.selectAll('.enemy').data(generateCirclePositions(numEnemies))
                            .classed('collided', false)
-                           .transition().duration(2000)
+                           .transition().duration(transitionTime)
                            .tween('Hi Oleg', function(d, i) {
                              var score = d3.select('.current').selectAll('span');
                              score.text(parseInt(score.text(), 10) + 1);
@@ -81,11 +67,10 @@ var updateEnemyPosition = function() {
                              var interpolateY = d3.interpolate(currentY, newY);
                              return function(t) {
                               // debugger;
-
                                var enemyX = interpolateX(t);
                                var enemyY = interpolateY(t);
                                if (!d3.select(this).classed('collided') && Math.abs(enemyX - (playerX + playingFieldSize.width / 2)) < 25 && Math.abs(enemyY - (playerY + playingFieldSize.height / 2)) < 25) {
-                                 console.log('COLLISSION !!');
+                                 console.log('Game Over');
                                  console.log(d3.select(this).classed('collided'));
                                  var currentCollisions = d3.select('.collisions').selectAll('span');
                                  currentCollisions.text(parseInt(currentCollisions.text(), 10) + 1);
@@ -103,49 +88,41 @@ var updateEnemyPosition = function() {
                            .attr('cy', function (d) { return d.yAxis; });
 };
 
-setInterval(updateEnemyPosition, 2000);
+// UPdate Enemy Location based on transitionTime
+setInterval(updateEnemyPosition, transitionTime);
 
-var newGame = true;
-var playerX = 0;
-var playerY = 0;
-
-var randomNum = function() {
-  return Math.floor(Math.random() * 101);
-};
-
-
-
-var dragstarted = function(d, i) {
-  console.log('You clicked a draggable!');
-  if (newGame) {
-    d.xAxis = 0;
-    d.yAxis = 0;
-    newGame = false;
-  }
-  
-};
-var dragged = function(d, i) {
-  console.log(`x-axis: ${d.xAxis} y-axis: ${d.yAxis}`);
-  d.xAxis += d3.event.dx;
-  d.yAxis += d3.event.dy; 
-  d3.select(this).attr('transform', function(d, i) {
-    return 'translate(' + [ d.xAxis, d.yAxis ] + ')';
-  });
-  playerX = d.xAxis;
-  playerY = d.yAxis;
-};
-var dragended = function() {
-  console.log('Mouse released!');
-};
-
+// Drag Definitions
 var drag = d3.behavior.drag()
     .origin(function(d) { return d; })
     .on('dragstart', dragstarted)
     .on('drag', dragged)
     .on('dragend', dragended);
 
-// Create the player
+var dragstarted = function(d, i) {
+  console.log('Hehe, that tickles');
+  if (newGame) {
+    d.xAxis = 0;
+    d.yAxis = 0;
+    newGame = false;
+  }
 
+};
+var dragged = function(d, i) {
+  // console.log(`x-axis: ${d.xAxis} y-axis: ${d.yAxis}`);
+  d.xAxis += d3.event.dx;
+  d.yAxis += d3.event.dy;
+  d3.select(this).attr('transform', function(d, i) {
+    return 'translate(' + [ d.xAxis, d.yAxis ] + ')';
+  });
+  playerX = d.xAxis;
+  playerY = d.yAxis;
+};
+// Not using this yet
+// var dragended = function() {
+//   console.log('Mouse released!');
+// };
+
+// Create the player
 var playerOptions = {'xAxis': playingFieldSize.width / 2, 'yAxis': playingFieldSize.height / 2, 'radius': 10, 'color': 'orange'};
 board.selectAll('.player').data([playerOptions])
                           .enter().append('circle')
@@ -155,14 +132,3 @@ board.selectAll('.player').data([playerOptions])
                           .style('fill', function(d) { return d.color; })
                           .classed('player', true)
                           .call(drag);
-
-
-
-
-
-
-
-
-
-
-
